c0fdad89e9ca6033977ea5e0075fb1c6cb31c24d
beruhigungsminister <morschermax@googlemail.com>
2025-12-10 17:27:10 +0100

    Player updated

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -17,8 +28,16 @@ def main():
                 return
     
         screen.fill("black")
+        
+        
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
+        dt = clock.tick(60)
+        dt /= 1000
 
 
 if __name__ == "__main__":
